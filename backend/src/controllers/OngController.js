d532eeb9e5595406dcc011a0043cb9d0b964265a
Bruno Sousa <brse01@gmail.com>
2020-03-24 21:12:20 -0300

    feat: Create backend

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,33 @@
+const connection = require('../database/connection');
+const crypto = require('crypto');
+
+module.exports = {
+    async index(request, response){
+        try {
+            const ongs = await connection('ongs').select('*');
+            return response.json(ongs);
+        } catch (error) {
+            return response.status(404).json({error: 'Error while listing ONG'});
+        }    
+    },
+    async store(request, response){
+        try {
+            const { name, email, whatsapp, city, uf } = request.body;
+    
+            const id = crypto.randomBytes(4).toString('HEX');
+            await connection('ongs').insert({
+                id,
+                name,
+                email,
+                whatsapp,
+                city,
+                uf
+            });
+    
+            return response.json({ id });
+        
+        } catch (error) {
+            return response.status(404).json({error: 'Error creating ONG'});
+        }        
+    }
+}
