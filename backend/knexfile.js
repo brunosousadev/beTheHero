8d779190ef80137f0946fedbf2d7615ff444db84
Bruno Sousa <brse01@gmail.com>
2020-04-03 23:15:56 -0300

    feat(ong): create test

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -7,7 +7,17 @@ module.exports = {
     connection: {
       filename: './src/database/db.sqlite'
     },
-    migrations: { 
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true,
+  },
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/test.sqlite'
+    },
+    migrations: {
       directory: './src/database/migrations'
     },
     useNullAsDefault: true,
